788809ae76a5db85ef3f05da89c6625404bda92e
leen8908 <eanli2000@gmail.com>
2023-05-07 20:35:16 +0800

    fix swipe microservice test

--- microservices/swipecard/test_microservices_swipe_card.py
+++ microservices/swipecard/test_microservices_swipe_card.py
@@ -1,355 +1,202 @@
-# import datetime
-# import os
-# import random
-# import string
-# import sys
-# import loguru
-# import pytest
-# from sqlalchemy import create_engine
-# from fastapi.encoders import jsonable_encoder
-# from fastapi.testclient import TestClient
-# from sqlalchemy.orm import scoped_session,sessionmaker
-# from sqlalchemy_utils.functions import create_database, database_exists
+import datetime
 
-# # SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # noqa E402
-# # sys.path.append(os.path.dirname(SCRIPT_DIR))
-# from microservices.swipecard.core.config import settings
-# from microservices.swipecard.swipe import app  # Flask instance of the API
-# from microservices.swipecard.swipe import get_db
+import pytest
+from fastapi.encoders import jsonable_encoder
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy_utils.functions import create_database, database_exists
 
-# from . import crud, schemas
-# from .database import Base
-# from .models.mr_liked_hated_member import MR_Liked_Hated_Member
-# from .models.mr_member import MR_Member
-# from .models.user import User
-# from .models.matching_room import MatchingRoom
+# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # noqa E402
+# sys.path.append(os.path.dirname(SCRIPT_DIR))
+from microservices.swipecard.core.config import settings
+from microservices.swipecard.swipe import app  # Flask instance of the API
+from microservices.swipecard.swipe import get_db
 
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@\
-# {settings.POSTGRES_HOST}:{settings.DATABASE_PORT}/test{settings.POSTGRES_DB}"
+from .database import Base
+from .models.matching_room import MatchingRoom
+from .models.mr_liked_hated_member import MR_Liked_Hated_Member
+from .models.mr_member import MR_Member
+from .models.user import User
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@\
+{settings.POSTGRES_HOST}:{settings.DATABASE_PORT}/test.db"
 
-# if not database_exists(SQLALCHEMY_DATABASE_URL):
-#     create_database(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
-# # Set up the database once
-# # Base.metadata.drop_all(bind=engine)
-# Base.metadata.create_all(bind=engine)
+if not database_exists(SQLALCHEMY_DATABASE_URL):
+    create_database(SQLALCHEMY_DATABASE_URL)
 
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Set up the database once
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-# @pytest.fixture(scope="module")
-# def db_conn():
-#     session = SessionLocal()
-#     try:
-#         yield session
-#     finally:
-#         session.close()
+@pytest.fixture(scope="module")
+def db_conn():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
 
 
-# @pytest.fixture(scope="module")
-# def client():
-#     def override_get_db():
-#         session = SessionLocal()
-#         try:
-#             yield session
-#         finally:
-#             session.close()
+@pytest.fixture(scope="module")
+def client():
+    def override_get_db():
+        session = SessionLocal()
+        try:
+            yield session
+        finally:
+            session.close()
 
-#     app.dependency_overrides[get_db] = override_get_db
-#     with TestClient(app) as client:
-#         yield client
-#     # reset overrides
-#     app.dependency_overrides = {}
-
-
-# # session = scoped_session(
-# #     sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# # )
-
-# # # Fake data
-# # # user
-# # for i in range(4):
-# #     email="test_admin" + str(i + 1) + "@sdm-teamatch.com"
-# #     name="test_admin" + str(i + 1)
-# #     is_google_sso=True
-# #     db_obj = User(
-# #             email=email,
-# #             password=None,
-# #             name=name,
-# #             image=None,
-# #             is_admin=False,
-# #             is_google_sso=is_google_sso,
-# #         )
-# #     session.add(db_obj)
-# #     session.commit()
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as client:
+        yield client
+    # reset overrides
+    app.dependency_overrides = {}
 
 
-# # # matching room
-# # db_obj = MatchingRoom(
-# #     name="swipe_test_matching_room",
-# #     room_id="swipe_test_matching_room001",
-# #     due_time=datetime.datetime.now(),
-# #     min_member_num=3,
-# #     description="",
-# #     is_forced_matching=True,
-# #     created_time=datetime.datetime.now()
-# # )
-# # session.add(db_obj)
-# # session.commit()
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-# # # MR_member
-# # user_uuids = []
-# # for i in range(4):
-# #     user =jsonable_encoder(session.query(User).filter(User.email == "test_admin" + str(i + 1) + "@sdm-teamatch.com").first())
-# #     user_uuids.append(user["user_uuid"])
-# # matching_room = session.query(MatchingRoom).filter(MatchingRoom.room_id == "swipe_test_matching_room001").first()
-# # room_uuid = jsonable_encoder(matching_room)["room_uuid"]
-# # for uuid in user_uuids:
-# #     mr_member_in1 = MR_Member(
-# #         user_uuid=uuid,
-# #         room_uuid=room_uuid,
-# #     )
-# #     session.add(mr_member_in1)
-# #     session.commit()
+# Fake data
+# user
+for i in range(4):
+    email = "test_admin" + str(i + 1) + "@sdm-teamatch.com"
+    name = "test_admin" + str(i + 1)
+    is_google_sso = True
+    db_obj = User(
+        email=email,
+        password=None,
+        name=name,
+        image=None,
+        is_admin=False,
+        is_google_sso=is_google_sso,
+    )
+    session.add(db_obj)
+    session.commit()
 
 
-# # def delete_fake_data():
-#     # delete MR_Liked_Hated_Member
-#     # obj = (
-#     #     session.query(MR_Liked_Hated_Member)
-#     #     .filter(MR_Liked_Hated_Member.room_uuid == room_uuid)
-#     #     .first()
-#     # )
-#     # session.delete(obj)
-#     # session.commit()
-#     # session.refresh()
-#     # # delete mr member
-#     # for user_uuid in user_uuids:
-#     #     obj = (
-#     #         session.query(MR_Member)
-#     #         .filter(MR_Member.user_uuid == user_uuid)
-#     #         .first()
-#     #     )
-#     #     session.delete(obj)
-#     #     session.commit()
-#     #     session.refresh()
-#     # # delete matching room
-#     # obj = (
-#     #     session.query(MatchingRoom)
-#     #     .filter(MatchingRoom.room_id == "swipe_test_matching_room001")
-#     #     .first()
-#     # )
-#     # session.delete(obj)
-#     # session.commit()
-#     # session.refresh()
-#     # # delete user
-#     # for user_uuid in user_uuids:
-#     #     obj = session.query(User).filter(User.user_uuid == user_uuid).first()
-#     #     session.delete(obj)
-#     #     session.commit()
-#     #     session.refresh()
-#     # db_obj = User(
-#     #         email="ifexistthenright",
-#     #         password=None,
-#     #         name="ifexistthenright",
-#     #         image=None,
-#     #         is_admin=False,
-#     #         is_google_sso=True,
-#     #     )
-#     # session.add(db_obj)
-#     # session.commit()
+# matching room
+db_obj = MatchingRoom(
+    name="swipe_test_matching_room",
+    room_id="swipe_test_matching_room001",
+    due_time=datetime.datetime.now(),
+    min_member_num=3,
+    description="",
+    is_forced_matching=True,
+    created_time=datetime.datetime.now(),
+)
+session.add(db_obj)
+session.commit()
+
+# MR_member
+user_uuids = []
+for i in range(4):
+    user = jsonable_encoder(
+        session.query(User)
+        .filter(User.email == "test_admin" + str(i + 1) + "@sdm-teamatch.com")
+        .first()
+    )
+    user_uuids.append(user["user_uuid"])
+matching_room = (
+    session.query(MatchingRoom)
+    .filter(MatchingRoom.room_id == "swipe_test_matching_room001")
+    .first()
+)
+room_uuid = jsonable_encoder(matching_room)["room_uuid"]
+for uuid in user_uuids:
+    mr_member_in1 = MR_Member(
+        user_uuid=uuid,
+        room_uuid=room_uuid,
+    )
+    session.add(mr_member_in1)
+    session.commit()
 
 
-# @pytest.fixture(scope="module")
-# def get_server_api():
-#     server_name = "http://localhost:8002"
-#     return server_name
+def delete_fake_data():
+    # delete MR_Liked_Hated_Member
+    obj = (
+        session.query(MR_Liked_Hated_Member)
+        .filter(MR_Liked_Hated_Member.room_uuid == room_uuid)
+        .first()
+    )
+    session.delete(obj)
+    session.commit()
+    # delete mr member
+    for user_uuid in user_uuids:
+        obj = session.query(MR_Member).filter(MR_Member.user_uuid == user_uuid).first()
+        session.delete(obj)
+        session.commit()
+    # delete matching room
+    obj = (
+        session.query(MatchingRoom)
+        .filter(MatchingRoom.room_id == "swipe_test_matching_room001")
+        .first()
+    )
+    session.delete(obj)
+    session.commit()
+    # delete user
+    for user_uuid in user_uuids:
+        obj = session.query(User).filter(User.user_uuid == user_uuid).first()
+        session.delete(obj)
+        session.commit()
 
 
-# # Test
-# def test_microservice_save_preference(get_server_api, db_conn, client):
-#     # Fake data
-#     # user
-#     for i in range(4):
-#         email="swipe_test_admin" + str(i + 1) + "@sdm-teamatch.com"
-#         name="test_admin" + str(i + 1)
-#         is_google_sso=True
-#         db_obj = User(
-#                 email=email,
-#                 password=None,
-#                 name=name,
-#                 image=None,
-#                 is_admin=False,
-#                 is_google_sso=is_google_sso,
-#             )
-#         db_conn.add(db_obj)
-#         try:
-#             db_conn.commit()
-#         except:
-#             db_conn.rollback()
-#             raise
-#         finally:
-#             db_conn.close()
+@pytest.fixture(scope="module")
+def get_server_api():
+    server_name = "http://localhost:8002"
+    return server_name
 
 
-#     # matching room
-#     db_obj = MatchingRoom(
-#         name="swipetest_matching_room",
-#         room_id="swipetest_matching_room001",
-#         due_time=datetime.datetime.now(),
-#         min_member_num=3,
-#         description="",
-#         is_forced_matching=True,
-#         created_time=datetime.datetime.now()
-#     )
-#     db_conn.add(db_obj)
-#     try:
-#         db_conn.commit()
-#     except:
-#         db_conn.rollback()
-#         raise
-#     finally:
-#         db_conn.close()
+# Test
+def test_microservice_save_preference(get_server_api, db_conn, client):
+    member = []
+    for i in range(2):
+        member.append(
+            jsonable_encoder(
+                db_conn.query(MR_Member)
+                .filter(MR_Member.user_uuid == user_uuids[i])
+                .first()
+            )
+        )
 
-#     # MR_member
-#     user_uuids = []
-#     for i in range(4):
-#         user =jsonable_encoder(db_conn.query(User).filter(User.email == "test_admin" + str(i + 1) + "@sdm-teamatch.com").first())
-#         user_uuids.append(user["user_uuid"])
-#     matching_room = db_conn.query(MatchingRoom).filter(MatchingRoom.room_id == "swipetest_matching_room001").first()
-#     room_uuid = jsonable_encoder(matching_room)["room_uuid"]
-#     for uuid in user_uuids:
-#         mr_member_in1 = MR_Member(
-#             user_uuid=uuid,
-#             room_uuid=room_uuid,
-#         )
-#         db_conn.add(mr_member_in1)
-#         try:
-#             db_conn.commit()
-#         except:
-#             db_conn.rollback()
-#             raise
-#         finally:
-#             db_conn.close()
+    preference = {
+        "member_id": member[0]["member_id"],
+        "room_uuid": room_uuid,
+        "target_member_id": member[1]["member_id"],
+        "is_like": True,
+        "is_hated": False,
+    }
 
-#     member = []
-#     for i in range(2):
-#         member.append(
-#             jsonable_encoder(
-#                 db_conn.query(MR_Member)
-#                 .filter(MR_Member.user_uuid == user_uuids[i])
-#                 .first()
-#             )
-#         )
+    response = client.post(
+        f"{get_server_api}/swipe-card/swipe",
+        json=preference,
+    )
 
-#     preference = {
-#         "member_id": member[0]["member_id"],
-#         "room_uuid": room_uuid,
-#         "target_member_id": member[1]["member_id"],
-#         "is_like": True,
-#         "is_hated": False,
-#     }
-
-#     response = client.post(
-#         f"{get_server_api}/swipe-card/swipe",
-#         json=preference,
-#     )
-
-#     assert response.status_code == 200
-#     assert response.json()["message"] == "success"
+    assert response.status_code == 200
+    assert response.json()["message"] == "success"
 
 
-# def test_microservice_get_recommendation(get_server_api, db_conn, client):
-#     member = jsonable_encoder(db_conn.query(MR_Member).first())
-#     matching_room = db_conn.query(MatchingRoom).filter(MatchingRoom.room_id == "swipetest_matching_room001").first()
-#     room_uuid = jsonable_encoder(matching_room)["room_uuid"]
-#     recommend_in = {"member_id": member["member_id"], "room_uuid": room_uuid}
+def test_microservice_get_recommendation(get_server_api, db_conn, client):
+    member = jsonable_encoder(db_conn.query(MR_Member).first())
 
-#     response = client.post(
-#         f"{get_server_api}/swipe-card/swipe-recommend",
-#         json=recommend_in,
-#     )
-#     response_data = response.json()["data"]
-#     rcmd_member_list = []
-#     for d in response_data:
-#         rcmd_member_list.append(d["recommended_member_id"])
+    recommend_in = {"member_id": member["member_id"], "room_uuid": room_uuid}
 
-#     # delete fake data
-#     #delete_fake_data()
-#     user_uuids = []
-#     for i in range(4):
-#         user =jsonable_encoder(db_conn.query(User).filter(User.email == "swipe_test_admin" + str(i + 1) + "@sdm-teamatch.com").first())
-#         user_uuids.append(user["user_uuid"])
-#     obj = (
-#         db_conn.query(MR_Liked_Hated_Member)
-#         .filter(MR_Liked_Hated_Member.room_uuid == room_uuid)
-#         .first()
-#     )
-#     db_conn.delete(obj)
-#     try:
-#         db_conn.commit()
-#     except:
-#         db_conn.rollback()
-#         raise
-#     finally:
-#         db_conn.close()
-#     db_conn.refresh()
-#     # delete mr member
-#     for user_uuid in user_uuids:
-#         obj = (
-#             db_conn.query(MR_Member)
-#             .filter(MR_Member.user_uuid == user_uuid)
-#             .first()
-#         )
-#         db_conn.delete(obj)
-#         try:
-#             db_conn.commit()
-#         except:
-#             db_conn.rollback()
-#             raise
-#         finally:
-#             db_conn.close()
-#         db_conn.refresh()
-#     # delete matching room
-#     obj = (
-#         db_conn.query(MatchingRoom)
-#         .filter(MatchingRoom.room_id == "swipetest_matching_room001")
-#         .first()
-#     )
-#     db_conn.delete(obj)
-#     try:
-#         db_conn.commit()
-#     except:
-#         db_conn.rollback()
-#         raise
-#     finally:
-#         db_conn.close()
-#     db_conn.refresh()
-#     # delete user
-#     for user_uuid in user_uuids:
-#         obj = db_conn.query(User).filter(User.user_uuid == user_uuid).first()
-#         db_conn.delete(obj)
-#         db_conn.commit()
-#         db_conn.refresh()
-#     db_obj = User(
-#             email="ifexistthenright",
-#             password=None,
-#             name="ifexistthenright",
-#             image=None,
-#             is_admin=False,
-#             is_google_sso=True,
-#         )
-#     db_conn.add(db_obj)
-#     try:
-#         db_conn.commit()
-#     except:
-#         db_conn.rollback()
-#         raise
-#     finally:
-#         db_conn.close()
+    response = client.post(
+        f"{get_server_api}/swipe-card/swipe-recommend",
+        json=recommend_in,
+    )
+    response_data = response.json()["data"]
+    rcmd_member_list = []
+    for d in response_data:
+        rcmd_member_list.append(d["recommended_member_id"])
 
-#     assert response.status_code == 200
-#     assert response.json()["message"] == "success"
-#     assert len(rcmd_member_list) == 2
+    # delete fake data
+    delete_fake_data()
+
+    assert response.status_code == 200
+    assert response.json()["message"] == "success"
+    assert len(rcmd_member_list) == 2
--- test/test_swipe_card.py
+++ test/test_swipe_card.py
@@ -1,20 +1,12 @@
-import datetime
 import random
 import string
-from datetime import timedelta
 
 import pytest
-from fastapi.encoders import jsonable_encoder
 
-from app import crud, schemas
-from app.core import security
 from app.core.config import settings
-from app.models.matching_room import MatchingRoom
 from app.models.mr_liked_hated_member import MR_Liked_Hated_Member
-from app.models.mr_member import MR_Member
-from app.models.user import User
 
-from .contest import db_conn, test_client, test_session
+from .contest import db_conn, get_user_authentication_headers, test_client
 
 # pytest fixture
 db_conn = db_conn
@@ -22,77 +14,77 @@ test_client = test_client
 
 # Fake data
 # user
-fake_user = []
-for i in range(4):
-    obj_in = schemas.UserCreate(
-        email="test_admin" + str(i + 1) + "@sdm-teamatch.com",
-        name="test_admin" + str(i + 1),
-        is_google_sso=True,
-    )
-    fake_user.append(obj_in)
-    crud.user.create(
-        test_session,
-        obj_in=obj_in,
-    )
-# matching room
-matching_room_in = schemas.MatchingRoomCreate(
-    name="swipe_test_matching_room",
-    room_id="swipe_test_matching_room001",
-    due_time=datetime.datetime.now(),
-    min_member_num=3,
-)
-crud.matching_room.create(test_session, obj_in=matching_room_in)
-# MR_member
-user_uuids = []
-for i in range(4):
-    user = crud.user.get_by_email(
-        test_session, email="test_admin" + str(i + 1) + "@sdm-teamatch.com"
-    )
-    user_uuids.append(user.user_uuid)
-matching_room = crud.matching_room.get_by_room_id(
-    test_session, room_id="swipe_test_matching_room001"
-)
-room_uuid = jsonable_encoder(matching_room)["room_uuid"]
-for uuid in user_uuids:
-    mr_member_in1 = MR_Member(
-        user_uuid=uuid,
-        room_uuid=room_uuid,
-    )
-    test_session.add(mr_member_in1)
-    test_session.commit()
+# fake_user = []
+# for i in range(4):
+#     obj_in = schemas.UserCreate(
+#         email="test_admin" + str(i + 1) + "@sdm-teamatch.com",
+#         name="test_admin" + str(i + 1),
+#         is_google_sso=True,
+#     )
+#     fake_user.append(obj_in)
+#     crud.user.create(
+#         test_session,
+#         obj_in=obj_in,
+#     )
+# # matching room
+# matching_room_in = schemas.MatchingRoomCreate(
+#     name="swipe_test_matching_room",
+#     room_id="swipe_test_matching_room001",
+#     due_time=datetime.datetime.now(),
+#     min_member_num=3,
+# )
+# crud.matching_room.create(test_session, obj_in=matching_room_in)
+# # MR_member
+# user_uuids = []
+# for i in range(4):
+#     user = crud.user.get_by_email(
+#         test_session, email="test_admin" + str(i + 1) + "@sdm-teamatch.com"
+#     )
+#     user_uuids.append(user.user_uuid)
+# matching_room = crud.matching_room.get_by_room_id(
+#     test_session, room_id="swipe_test_matching_room001"
+# )
+# room_uuid = jsonable_encoder(matching_room)["room_uuid"]
+# for uuid in user_uuids:
+#     mr_member_in1 = MR_Member(
+#         user_uuid=uuid,
+#         room_uuid=room_uuid,
+#     )
+#     test_session.add(mr_member_in1)
+#     test_session.commit()
 
 
-def delete_fake_data():
-    # delete MR_Liked_Hated_Member
-    obj = (
-        test_session.query(MR_Liked_Hated_Member)
-        .filter(MR_Liked_Hated_Member.room_uuid == room_uuid)
-        .first()
-    )
-    test_session.delete(obj)
-    test_session.commit()
-    # delete mr member
-    for user_uuid in user_uuids:
-        obj = (
-            test_session.query(MR_Member)
-            .filter(MR_Member.user_uuid == user_uuid)
-            .first()
-        )
-        test_session.delete(obj)
-        test_session.commit()
-    # delete matching room
-    obj = (
-        test_session.query(MatchingRoom)
-        .filter(MatchingRoom.room_id == "swipe_test_matching_room001")
-        .first()
-    )
-    test_session.delete(obj)
-    test_session.commit()
-    # delete user
-    for user_uuid in user_uuids:
-        obj = test_session.query(User).filter(User.user_uuid == user_uuid).first()
-        test_session.delete(obj)
-        test_session.commit()
+# def delete_fake_data():
+#     # delete MR_Liked_Hated_Member
+#     obj = (
+#         test_session.query(MR_Liked_Hated_Member)
+#         .filter(MR_Liked_Hated_Member.room_uuid == room_uuid)
+#         .first()
+#     )
+#     test_session.delete(obj)
+#     test_session.commit()
+#     # delete mr member
+#     for user_uuid in user_uuids:
+#         obj = (
+#             test_session.query(MR_Member)
+#             .filter(MR_Member.user_uuid == user_uuid)
+#             .first()
+#         )
+#         test_session.delete(obj)
+#         test_session.commit()
+#     # delete matching room
+#     obj = (
+#         test_session.query(MatchingRoom)
+#         .filter(MatchingRoom.room_id == "swipe_test_matching_room001")
+#         .first()
+#     )
+#     test_session.delete(obj)
+#     test_session.commit()
+#     # delete user
+#     for user_uuid in user_uuids:
+#         obj = test_session.query(User).filter(User.user_uuid == user_uuid).first()
+#         test_session.delete(obj)
+#         test_session.commit()
 
 
 @pytest.fixture(scope="module")
@@ -105,35 +97,42 @@ def random_lower_string():
     return "".join(random.choices(string.ascii_lowercase, k=32))
 
 
-def get_user_authentication_headers(session, email):
-    email = email
+# def get_user_authentication_headers(session, email):
+#     email = email
 
-    user = crud.user.get_by_email(db=session, email=email)
-    user = jsonable_encoder(user)
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = security.create_access_token(
-        user["user_uuid"], expires_delta=access_token_expires
-    )
-    headers = {"Authorization": f"Bearer {access_token}"}
-    return headers
+#     user = crud.user.get_by_email(db=session, email=email)
+#     user = jsonable_encoder(user)
+#     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+#     access_token = security.create_access_token(
+#         user["user_uuid"], expires_delta=access_token_expires
+#     )
+#     headers = {"Authorization": f"Bearer {access_token}"}
+#     return headers
 
 
 # Test
 def test_save_preference_who_has_logged_in(get_server_api, db_conn, test_client):
-    member = []
-    for i in range(2):
-        member.append(
-            jsonable_encoder(
-                db_conn.query(MR_Member)
-                .filter(MR_Member.user_uuid == user_uuids[i])
-                .first()
-            )
-        )
+    # member = []
+    # for i in range(2):
+    #     member.append(
+    #         jsonable_encoder(
+    #             db_conn.query(MR_Member)
+    #             .filter(MR_Member.user_uuid == user_uuids[i])
+    #             .first()
+    #         )
+    #     )
 
+    # preference = {
+    #     "member_id": member[0]["member_id"],
+    #     "room_uuid": room_uuid,
+    #     "target_member_id": member[1]["member_id"],
+    #     "is_like": True,
+    #     "is_hated": False,
+    # }
     preference = {
-        "member_id": member[0]["member_id"],
-        "room_uuid": room_uuid,
-        "target_member_id": member[1]["member_id"],
+        "member_id": 2,
+        "room_uuid": "15b14b78-7b33-4274-868f-e3aca152bd80",
+        "target_member_id": 11,
         "is_like": True,
         "is_hated": False,
     }
@@ -141,10 +140,22 @@ def test_save_preference_who_has_logged_in(get_server_api, db_conn, test_client)
     response = test_client.post(
         f"{get_server_api}{settings.API_V1_STR}/swipe-card/swipe",
         headers=get_user_authentication_headers(
-            session=db_conn, email="test_admin1@sdm-teamatch.com"
+            session=db_conn, email="admin@sdm-teamatch.com"
         ),
         json=preference,
     )
+
+    # delete mr_liked_hated_member
+    mr_liked_hated_member = (
+        db_conn.query(MR_Liked_Hated_Member)
+        .filter(
+            MR_Liked_Hated_Member.member_id == 2,
+            MR_Liked_Hated_Member.target_member_id == 11,
+        )
+        .first()
+    )
+    db_conn.delete(mr_liked_hated_member)
+    db_conn.commit()
 
     assert response.status_code == 200
     assert response.json()["message"] == "success"
@@ -168,14 +179,15 @@ def test_save_preference_who_has_not_logged_in(get_server_api, test_client):
 
 
 def test_get_recommendation(get_server_api, db_conn, test_client):
-    member = jsonable_encoder(db_conn.query(MR_Member).first())
+    # member = jsonable_encoder(db_conn.query(MR_Member).first())
 
-    recommend_in = {"member_id": member["member_id"], "room_uuid": room_uuid}
+    # recommend_in = {"member_id": member["member_id"], "room_uuid": room_uuid}
+    recommend_in = {"member_id": 3, "room_uuid": "58102867-1773-4c68-a78f-d6da7124bb2d"}
 
     response = test_client.post(
         f"{get_server_api}{settings.API_V1_STR}/swipe-card/swipe-recommend",
         headers=get_user_authentication_headers(
-            session=db_conn, email="test_admin1@sdm-teamatch.com"
+            session=db_conn, email="admin@sdm-teamatch.com"
         ),
         json=recommend_in,
     )
@@ -185,7 +197,7 @@ def test_get_recommendation(get_server_api, db_conn, test_client):
         rcmd_member_list.append(d["recommended_member_id"])
 
     # delete fake data
-    delete_fake_data()
+    # delete_fake_data()
 
     assert response.status_code == 200
     assert response.json()["message"] == "success"
