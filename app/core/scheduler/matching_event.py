09347187f90cf9ffa7a2cbc86b1b112400806e29
Pin-HsuanWu <r11725058@ntu.edu.tw>
2023-05-09 23:06:47 +0800

    Add Micro service scheduler

--- app/core/scheduler/matching_event.py
+++ app/core/scheduler/matching_event.py
@@ -12,7 +12,8 @@ import json
 import requests
 from sqlalchemy.orm import Session
 from fastapi import Depends
-from tzlocal import get_localzone
+import pytz
+
 
 """
 Matching event scheduler
@@ -20,8 +21,9 @@ Matching event scheduler
 scheduler = BackgroundScheduler()
 scheduler.start()
 
-async def matching_event(matching_room: MatchingRoom, db: Session):
+def matching_event(matching_room: MatchingRoom, db: Session):
     logger.info("run matching_event function")
+    logger.info(matching_room.room_id)
     """
     Call matching event micro-service
     """
@@ -45,54 +47,65 @@ async def matching_event(matching_room: MatchingRoom, db: Session):
     """
     Create Group and GR_Member
     """
-    # Get notify template_uuid
-    notification_template = crud.notification_template.get_by_template_id(
-        db=db, template_id="matching_result"
-    )
+    # # Get notify template_uuid
+    # notification_template = crud.notification_template.get_by_template_id(
+    #     db=db, template_id="matching_result"
+    # )
 
-    group_list = []
-    group_id = 0
-    for group in result:
-        # Create Group
-        group_id += 1
-        new_group_schema = schemas.GroupCreate(
-            name=matching_room.name + "_" + str(group_id),
-            group_id=matching_room.room_id + "_" + str(group_id),
-            room_uuid=matching_room.room_uuid,
-        )
-        new_group = crud.group.create(db=db, obj_in=new_group_schema)
+    # group_list = []
+    # group_id = 0
+    # for group in result:
+    #     # Create Group
+    #     group_id += 1
+    #     new_group_schema = schemas.GroupCreate(
+    #         name=matching_room.name + "_" + str(group_id),
+    #         group_id=matching_room.room_id + "_" + str(group_id),
+    #         room_uuid=matching_room.room_uuid,
+    #     )
+    #     new_group = crud.group.create(db=db, obj_in=new_group_schema)
 
-        gr_mem_list = []
-        for gr_member in result[group]:
-            # Create GR_member
-            new_gr_mem_schema = schemas.GR_MemberCreate(
-                member_id=gr_member,
-                group_uuid=new_group.group_uuid,
-                join_time=new_group.create_time,
-            )
-            new_gr_mem = crud.gr_member.create(db=db, obj_in=new_gr_mem_schema)
+    #     gr_mem_list = []
+    #     for gr_member in result[group]:
+    #         # Create GR_member
+    #         new_gr_mem_schema = schemas.GR_MemberCreate(
+    #             member_id=gr_member,
+    #             group_uuid=new_group.group_uuid,
+    #             join_time=new_group.create_time,
+    #         )
+    #         new_gr_mem = crud.gr_member.create(db=db, obj_in=new_gr_mem_schema)
 
-            """
-            Call notification method for every Group Member
-            """
-            gr_user = crud.mr_member.get_by_member_id(
-                db=db, member_id=new_gr_mem.member_id
-            )
-            gr_mem_list.append(gr_user.member_id)
+    #         """
+    #         Call notification method for every Group Member
+    #         """
+    #         gr_user = crud.mr_member.get_by_member_id(
+    #             db=db, member_id=new_gr_mem.member_id
+    #         )
+    #         gr_mem_list.append(gr_user.member_id)
 
-            # Create notify send object
-            notification_send_object = schemas.NotificationSendObjectModel(
-                receiver_uuid=gr_user.user_uuid,
-                template_uuid=notification_template.template_uuid,
-                f_string=matching_room.name,
-            )
-            await notify(db, notification_send_object)
-        group_list.append(gr_mem_list)
+    #         # Create notify send object
+    #         notification_send_object = schemas.NotificationSendObjectModel(
+    #             receiver_uuid=gr_user.user_uuid,
+    #             template_uuid=notification_template.template_uuid,
+    #             f_string=matching_room.name,
+    #         )
+    #         notify(db, notification_send_object)
+    #     group_list.append(gr_mem_list)
     return
 
 def schedule_matching_event(matching_room: MatchingRoom):
     logger.info('schedule matching event')
+    logger.info(matching_room)
     db: Session = Depends(deps.get_db)
+
+    if scheduler.running:
+        logger.info('scheduler running')
+
+    # Time zone
+    utc_time = matching_room.due_time.astimezone(pytz.utc)
+    # Convert UTC timezone to local timezone
+    local_tz = pytz.timezone('Asia/Taipei')
+    local_time = utc_time.astimezone(local_tz)
+
     # call matching_event function
     scheduler.add_job(
         matching_event,
@@ -101,6 +114,7 @@ def schedule_matching_event(matching_room: MatchingRoom):
         run_date=matching_room.due_time,
         args=[matching_room, db]
     )
+
     return
 
 
@@ -113,4 +127,5 @@ def schedule_matching_room(mapper, connection, matching_room):
 
 @atexit.register
 def shutdown_scheduler():
+    logger.info('scheduler shutdown')
     scheduler.shutdown()
